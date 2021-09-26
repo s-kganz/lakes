ccf98120a54c6cce9f71eb3edc4af8fd6509445b
s-kganz <ganzkeenan1@gmail.com>
2021-09-26 19:21:08 -0400

    add 2007 nla data to workflow

--- scripts/join_keyvars_polys.R
+++ scripts/join_keyvars_polys.R
@@ -1,43 +1,56 @@
 library(tidyverse)
 library(sf)
 library(sp)
+library(lwgeom)
 library(magrittr)
 library(tidyjson)
 
-keyvars <- read_csv("data_in/nla_2012_sites_sampled/nla12_keyvariables_data.txt")
-keyvars_summary <- keyvars %>%
+# depth information for NLA12 lakes
+nla12_keyvars <- read_csv("data_in/nla/nla2012/nla12_keyvariables_data.txt")
+nla12_keyvars_summary <- nla12_keyvars %>%
   group_by(SITE_ID) %>%
   summarize(
-    index_depth_m = mean(INDEX_SITE_DEPTH),
+    maxdepth = mean(INDEX_SITE_DEPTH),
     elev_m = first(ELEVATION),
     index_lat = first(INDEX_LAT_DD),
     index_lon = first(INDEX_LON_DD)
   ) %>%
   drop_na() %>%
-  rename(NLA12_ID = SITE_ID)
+  rename(NLAID = SITE_ID)
 
-lakepolys <- st_read("data_in/nla_2012_sites_sampled/NLA2012_Sampled_Polys_20131217.shp") %>%
-  mutate(area_sqkm = as.numeric(st_area(geometry)) / 1e6)
+# depth information for NLA07 lakes
+nla07_depth <- read_csv("data_in/nla/nla2007/nla2007_sampledlakeinformation_20091113.csv") %>%
+  select(SITE_ID, DEPTHMAX, LAT_DD, LON_DD, ELEV_PT) %>%
+  rename(maxdepth = DEPTHMAX,
+         NLAID = SITE_ID,
+         index_lat = LAT_DD,
+         index_lon = LON_DD,
+         elev_m = ELEV_PT) %>%
+  group_by(NLAID) %>%
+  summarize_all(mean)
 
-lakeslope <- read_csv("data_working/nla12_median_slope.csv") %>%
-  rename(buffer_median_slope = median) %>%
-  select(-.geo)
+# merge the depths
+nla_depth <- bind_rows(nla12_keyvars_summary, nla07_depth) %>%
+  drop_na()
 
-lakegrad <- read_csv("data_working/nla12_gradient.csv") %>%
-  rename(buffer_min_elev=min, buffer_max_elev=max) %>%
-  select(-.geo)
+process_gee_csv <- function(path) {
+  csv <- read_csv(path, col_types = cols(NLA12_ID=col_character(), SITEID=col_character()))
+  label <- str_c(csv$stat[1], csv$length[1], "_")
+  csv %>%
+    rename_with(~str_c(label, .), .cols=c(max, median, min, contains("p"))) %>%
+    select(-`system:index`, -NLA12_ID, -SITEID, -.geo, -length, -stat) %>%
+    return()
+}
 
-lakebbox <- st_read("data_working/nla2012_oriented_bbox.shp") %>%
-  rename(bbox_width = width,
-         bbox_height = height,
-         bbox_area = area,
-         bbox_angle = angle,
-         bbox_perimeter = perimeter,
-         NLA12_ID = SITE_ID) %>%
-  select(-geometry)
+gee_csvs <- list.files(path="data_in/gee/", full.names=T)
+gee_csvs <- gee_csvs[!str_detect(gee_csvs, "geo")]
 
-lakegeo <- read_csv("data_working/nla2012_1000m_geo.csv") %>%
-  select(-.geo, -`system:index`)
+gee_join <- NULL
+for (gee in gee_csvs) {
+  new_csv <- process_gee_csv(gee)
+  if (is.null(gee_join)) { gee_join <- new_csv }
+  else {gee_join <- inner_join(gee_join, new_csv, by="NLAID")}
+}
 
 parse_geo_string <- function(str) {
   # parse the geology histogram
@@ -60,15 +73,19 @@ parse_geo_string <- function(str) {
   return(ret_list)
 }
 
+lakegeo <- read_csv("data_in/gee/geo1000.csv",
+                    col_types = cols(NLA12_ID=col_character())) %>%
+  select(NLAID, histogram)
+
 # convert the EE dictionary string into a list-column
-lakegeo$histogram <- map(lakegeo$histogram, parse_geo_string)
+lakegeo$histogram_parsed <- map(lakegeo$histogram, parse_geo_string)
 
 # spread the column into rows
 lakegeo <- lakegeo %>%
-  unnest(histogram) %>%
-  mutate(value = as.numeric(flatten_chr(histogram)),
-         key   = names(histogram)) %>%
-  select(-histogram) %>%
+  unnest(histogram_parsed) %>%
+  mutate(value = as.numeric(flatten_chr(histogram_parsed)),
+         key   = names(histogram_parsed)) %>%
+  select(-histogram_parsed) %>%
   mutate(key = str_c("geo_type", key)) %>%
   spread(key, value, fill=0) %>%
   # remove a zero column
@@ -76,49 +93,26 @@ lakegeo <- lakegeo %>%
   # normalize within each row
   mutate(row_sum = rowSums(select(., contains("geo_type"))),
          across(.cols=contains("geo_type"), ~ .x / row_sum)) %>%
-  select(-row_sum)
+  select(-row_sum, -histogram)
 
-lakes_join <- inner_join(keyvars_summary, lakepolys,
-                         by="NLA12_ID") %>%
-  inner_join(lakebbox, by="NLA12_ID") %>%
-  inner_join(lakeslope, by="NLA12_ID") %>%
-  inner_join(lakegrad, by="NLA12_ID") %>%
-  inner_join(lakegeo, by="NLA12_ID") %>%
-  select(NLA12_ID, contains("bbox"), contains("geo_type"), median_slope, 
-         area_sqkm, index_depth_m, buffer_min_elev, buffer_max_elev, PERIM_KM_1) %>%
-  rename(perimeter_km = PERIM_KM_1)
+lakepolys <- st_read("data_working/nla_combined.shp") %>%
+  select(-SITEID, -NLA12_ID)
+lakebbox  <- st_read("data_working/nla_combined_oriented_bbox.shp") %>%
+  tibble() %>%
+  select(-geometry, -SITEID, -NLA12_ID)
+names(lakebbox)[2:6] <- str_c("bbox_", names(lakebbox[2:6]))
 
-# Hypothesis: max slope w/in 100m of the lake explains depth moreso than area
-ggplot(lakes_join, aes(x=median_slope, y=index_depth_m)) + geom_point()
-ggplot(lakes_join, aes(x=area_sqkm, y=index_depth_m)) + geom_point() +
-  scale_x_log10()
-cor(log10(lakes_join$area_sqkm), lakes_join$index_depth_m)
-cor(lakes_join$median_slope, lakes_join$index_depth_m)
+nla_join <- lakepolys %>%
+  inner_join(lakebbox, by="NLAID") %>%
+  inner_join(lakegeo,  by="NLAID") %>%
+  inner_join(gee_join, by="NLAID") %>%
+  inner_join(nla_depth, by="NLAID") %>%
+  mutate(lakearea_m2 = st_area(geometry),
+         lakeperim_m = st_perimeter(geometry),
+         dev_index = 0.5 * lakeperim_m  / sqrt(lakearea_m2 * pi)) %>%
+  rowwise() %>%
+  mutate(bbox_mindim = min(bbox_width, bbox_height),
+         bbox_chardim = 0.5 * bbox_mindim * (lakearea_m2 / bbox_area))
 
-# Calculate characteristic dimension from the bbox
-# CD = min_bbox_dimension * (lake_area/bbox_area) * 0.5
-lakes_join <- lakes_join %>% mutate(
-  min_bbox_dim = pmin(bbox_width, bbox_height),
-  bbox_area_sqkm = bbox_area/1e6,
-  char_dim = min_bbox_dim * (area_sqkm / bbox_area_sqkm) * 0.5,
-  pred_max_depth = sin(median_slope * 2*pi / 180) * char_dim
-)
-
-ggplot(lakes_join, aes(x=pred_max_depth, index_depth_m)) + geom_point()
-cor(lakes_join$pred_max_depth, lakes_join$index_depth_m)
-
-# Calculate gradient within the buffer zone around the lake
-lakes_join <- lakes_join %>% mutate(
-  grad_100m = buffer_max_elev - buffer_min_elev
-)
-ggplot(lakes_join, aes(x=grad_100m, y=index_depth_m)) + geom_point()
-cor(lakes_join$index_depth_m, lakes_join$grad_100m)
-
-# Calculate shoreline development index
-lakes_join <- lakes_join %>% mutate(
-  dev_index = perimeter_km / (2*sqrt(pi * area_sqkm))
-)
-
-# save the data as a regular CSV since we threw out the geometry already
-lakes_join %>%
-  write_csv("data_out/nla2012_lakes_join.csv")
+nla_join %>%
+  st_write("data_out/nla_gee.geojson", delete_dsn=T)
