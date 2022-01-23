3f873822eb10c7441e246231de336992336f80fd
s-kganz <ganzkeenan1@gmail.com>
2022-01-22 20:54:37 -0500

    try using the modis monthly median for lake temp, clean up gcs to ee script

--- scripts/gee/calc_lake_temp.py
+++ scripts/gee/calc_lake_temp.py
@@ -7,37 +7,61 @@ from util import get_asset_list, create_export_tasks, run_export_tasks
 
 ee.Initialize()
 
-# for shifting LST into Kelvin
-scale  = 0.00341802
-offset = 149
+landsat = ee.ImageCollection("LANDSAT/LC08/C01/T1")
+modis   = ee.Image("OpenLandMap/CLM/CLM_LST_MOD11A2-DAY_M/v01")
 
-landsat = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+def calibrateRadWithProperties(img):
+  return ee.Algorithms.Landsat.calibratedRadiance(img) \
+    .set("DATE_ACQUIRED", img.get("DATE_ACQUIRED"))
 
-def make_lst_image(img):
-    global scale, offset
-    # mask out clouds
-    lst_mask = img.select("QA_PIXEL").bitwiseAnd(ee.Image.constant(64)).gt(0)
+def inversePlanck(img):
+  # convert radiance to blackbody temperature
+  # https:#ncc.nesdis.noaa.gov/data/planck.html
+  um = ee.Image.constant(10.9) # wavelength in um for B10
+  c1     = ee.Image.constant(1.191042e8)
+  c2     = ee.Image.constant(1.4387752e4)
+  
+  lst = c2.divide(
+    um.multiply(
+      c1.divide(um.pow(ee.Image.constant(5)).multiply(img)).add(1).log()
+    )
+  )
+  
+  lst = lst.set("system:time_start", img.get("DATE_ACQUIRED"))
+  return lst
 
-    return img.select("ST_B10").multiply(scale).add(offset).updateMask(lst_mask)
+def cloudMask(img):
+  # the cloud bit is #4, 2^4 = 16
+  return img.updateMask(
+    img.select("BQA").bitwiseAnd(ee.Image.constant(16)).eq(0)
+  )
+
+# Spring turnover anomaly
+lst_anomaly = landsat.filter(ee.Filter.calendarRange(3, 5, "month")) \
+  .map(cloudMask) \
+  .map(
+    calibrateRadWithProperties
+  ).select("B10").map(
+    inversePlanck
+  ).mean() \
+  .subtract(
+    modis.select(["may", "apr", "mar"]).reduce(ee.Reducer.mean())\
+      .multiply(0.02).focal_median(3) # kernel radius in px, effectively 3km
+  )
 
 def do_one_export(asset):
-    global landsat
+    global lst_anomaly
     features = ee.FeatureCollection(asset)
-    mean_lst = landsat.filter(ee.Filter.eq("PROCESSING_LEVEL", "L2SP")) \
-    .filter(ee.Filter.calendarRange(10, 11, "month")) \
-    .filterBounds(features) \
-    .map(make_lst_image) \
-    .reduce(ee.Reducer.mean())
 
-    return ee.Image(mean_lst).reduceRegions(
+    return lst_anomaly.reduceRegions(
         collection=features,
         reducer=ee.Reducer.first(),
         scale=30
     ).map(lambda feature: feature.setGeometry(None))
 
-assets = get_asset_list("projects/lagos-lakes/assets/lagos_us_points_huc4")
+assets = get_asset_list("projects/lagos-lakes/assets/lagos_us_points_huc8")
 tasks  = create_export_tasks(
     assets, ee.FeatureCollection, 
-    do_one_export, "lagos_us_temp_by_huc4"
+    do_one_export, "lagos_us_temp_by_huc8"
 )
 run_export_tasks(tasks)
--- scripts/gee/gcs_to_ee.py
+++ scripts/gee/gcs_to_ee.py
@@ -2,15 +2,15 @@ from glob import glob
 import subprocess
 import os
 
-bucket_base = "gs://lagos-200m-strip-shp/by_huc8/"
-asset_base  = "projects/lagos-lakes/assets/lagos_us_200m_strips/"
+bucket_base = "gs://huc8_points/"
+asset_base  = "projects/lagos-lakes/assets/lagos_us_points_huc8/"
+local_shp_path = "data_working/lagosus/points_by_huc8/*.shp"
 
 shps = map(
     lambda x: os.path.split(x)[1].replace(".shp", ""), 
-    glob("data_working/lagosus/by_huc8/*.shp")
+    glob(local_shp_path)
 )
 
-shps = ['hu8_zoneid_hu8_1638', 'hu8_zoneid_hu8_1777']
 for shp in shps:
     # create the earthengine call
     shp_source = bucket_base + shp + ".shp"
