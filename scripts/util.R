2bccf256c2942763bca26b4df4ea85642803e151
s-kganz <ganzkeenan1@gmail.com>
2022-03-03 10:05:45 -0500

    add max's forel-ule scale function to util

--- scripts/util.R
+++ scripts/util.R
@@ -1,6 +1,7 @@
 # This file is exclusively for helper functions.
 
 library(tidyverse)
+library(colorscience)
 
 parse_huc <- function(s) {
   # convert a possibly mangled HUC string to its correct 12-char string
@@ -41,4 +42,42 @@ safe_log_transform <- function(x) {
   eps <- 1 # minimum output is 0
   x <- x - min(x) + eps
   log(x)
+}
+
+fui.hue <- function(R, G, B) {
+  
+  # Convert R,G, and B spectral reflectance to dominant wavelength based
+  # on CIE chromaticity color space
+  
+  # see Wang et al 2015. MODIS-Based Radiometric Color Extraction and
+  # Classification of Inland Water With the Forel-Ule
+  # Scale: A Case Study of Lake Taihu
+  
+  require(colorscience)
+  # chromaticity.diagram.color.fill()
+  Xi <- 2.7689*R + 1.7517*G + 1.1302*B
+  Yi <- 1.0000*R + 4.5907*G + 0.0601*B
+  Zi <- 0.0565*G + 5.5943*B
+  
+  # calculate coordinates on chromaticity diagram
+  x <-  Xi / (Xi + Yi +  Zi)
+  y <-  Yi / (Xi + Yi +  Zi)
+  z <-  Zi / (Xi + Yi +  Zi)
+  
+  # calculate hue angle
+  alpha <- atan2( (x - 0.33), (y - 0.33)) * 180/pi
+  
+  # make look up table for hue angle to wavelength conversion
+  cie <- cccie31 %>%
+    mutate(a = atan2( (x - 0.33), (y - 0.33)) * 180/pi) %>%
+    dplyr::filter(wlnm <= 700) %>%
+    dplyr::filter(wlnm >=380)
+  
+  # find nearest dominant wavelength to hue angle
+  wl <- cie[as.vector(sapply(alpha,function(x) which.min(abs(x -
+                                                               cie$a)))), 'wlnm']
+  
+  #out <- cbind(as.data.frame(alpha), as.data.frame(wl))
+  
+  return(wl)
 }
