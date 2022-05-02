7306e4da56ae8b3b683f14fba66e4506c2f1bccc
s-kganz <ganzkeenan1@gmail.com>
2022-05-02 09:38:49 -0400

    add column names prefixes for satellite data taken over the whole polygon

--- scripts/clean_gee_reflectance.R
+++ scripts/clean_gee_reflectance.R
@@ -6,17 +6,18 @@ source("scripts/util.R")
 
 id_col <- "lagoslakei"
 
-files <- list.files("data_in/gee/lagos_us_reflectance", "*.csv",
+files <- list.files("data_in/gee/lagos_us_polygon_reflectance", "*.csv",
                     include.dirs=T, full.names=T)
 
 gee_reflectance <- vroom(files)
 
 gee_reflectance %>%
-  select(-`system:index`, -hu8_zoneid, -.geo) %>%
+  select(-`system:index`, -hu4_zoneid, -.geo) %>%
   # log transform all five bands
   mutate(across(contains("B"), log)) %>%
   # compute all possible band ratios
   mutate(pairwise(contains("B"),
                   list(ratio="/"))) %>%
   select(-B1, -B2, -B3, -B4, -B5) %>%
-  write_csv("data_out/lagos_us_reflectance.csv")
+  rename_with(~str_c("polygon_", .), everything()) %>%
+  write_csv("data_out/lagos_us_polygon_reflectance.csv")
--- scripts/clean_gee_temperature.R
+++ scripts/clean_gee_temperature.R
@@ -3,7 +3,7 @@ library(vroom)
 
 source("scripts/util.R")
 
-files <- list.files("data_in/gee/lagos_us_temperature_anomaly/", "*.csv",
+files <- list.files("data_in/gee/lagos_us_polygon_temperature_anomaly/", "*.csv",
                     include.dirs=T, full.names=T)
 
 # Sometimes we get unlucky and every point in a file has no imagery. In that
@@ -11,9 +11,10 @@ files <- list.files("data_in/gee/lagos_us_temperature_anomaly/", "*.csv",
 gee_temp    <- map_df(files, ~vroom(.x, progress=FALSE, show_col_types=FALSE))
 
 gee_wide <- gee_temp %>%
-  select(lagoslakei, first, month) %>%
+  select(lagoslakei, median, month) %>%
   rename(lagoslakeid=lagoslakei,
-         anomaly=first) %>%
+         anomaly=median) %>%
   mutate(month = str_c("t_anomaly_month_", month)) %>%
   pivot_wider(names_from=month, values_from=anomaly) %>%
-  write_csv("data_out/lagos_us_temperature.csv")
+  rename_with(~str_c("polygon_", .), everything()) %>%
+  write_csv("data_out/lagos_us_polygon_temperature.csv")
