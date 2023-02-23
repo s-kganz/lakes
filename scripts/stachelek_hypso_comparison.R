cafb29c80a80de2acb2faa9fb48b02d5c291eff2
s-kganz <ganzkeenan1@gmail.com>
2023-02-22 17:40:29 -0800

    do more comparison with stachelek paper

--- scripts/stachelek_hypso_comparison.R
+++ scripts/stachelek_hypso_comparison.R
@@ -6,7 +6,7 @@ hypso <- read_csv("data_in/stachelek_hypso/hypso.csv") %>%
   left_join(
     read_csv("data_out/lagos_us_shape.csv") %>% select(lagoslakei, area),
     by=c("llid"="lagoslakei"))
-obs_df <- read_csv("data_out/model_results/compiled_predictions.csv")
+obs_df <- read_csv("data_out/model_results/maxdepth/compiled_predictions.csv")
 stachelek_ids <- unique(hypso$llid)
 
 obs_df %>%
@@ -16,7 +16,7 @@ obs_df %>%
            form_factor >= 1.5 ~ "concave"
          )) %>%
   pull(shapetype) %>% table()
-  ggplot(aes(x=form_factor)) + geom_histogram()
+  #ggplot(aes(x=form_factor)) + geom_histogram()
 
 calc_zmean <- function(area_pct, depth_pct) {
   # sort both vectors in order of increasing area
@@ -36,6 +36,7 @@ hypso_ff <- hypso %>%
             zmean = hypso_int * zmax) %>%
   mutate(form_factor = zmean * 3 / zmax,
          shapetype = ifelse(form_factor < 1.5, "convex", "concave"))
+
 hypso_ff %>%  
   # it matches
   pull(shapetype) %>% table()
@@ -46,22 +47,48 @@ sample_weights <- hypso_ff %>%
   filter(area < 1e7) %>%
   mutate(sizeclass=floor(log10(area))) %>%
   group_by(sizeclass) %>%
-  summarize(sample_weight = n() / nrow(.))
+  summarize(sample_count = n() / nrow(.))
 
-obs_df %>% 
+group_sizes <- nrow(hypso_ff) * sample_weights$sample_count
+
+obs_df_sample <- obs_df %>% 
   filter(area < 1e7) %>%
+  filter(!(lagoslakeid %in% stachelek_ids),
+         is.na(lake_maxdepth_m)) %>%
   mutate(form_factor = 3 * best_meandepth / best_maxdepth,
          sizeclass=floor(log10(area))) %>%
   left_join(sample_weights, by="sizeclass") %>%
-  slice_sample(n=length(stachelek_ids), weight_by=.$sample_weight) %>%
-  select(lagoslakeid, form_factor) %>%
-  mutate(group="modeled") %>%
-  # compare the distribution of form factors with their real data
-  rbind(
-    hypso_ff %>% 
-      select(llid, form_factor) %>%
-      rename(lagoslakeid=llid) %>%
-      mutate(group="observed")
-  ) %>%
+  group_split(sizeclass) %>%
+  map2_dfr(group_sizes, ~slice_sample(.x, n=.y)) %>%
+  select(lagoslakeid, form_factor, area, sizeclass) %>%
+  mutate(group="RF modeled lakes")
+  
+table(obs_df_sample$sizeclass)
+
+stachelek_sample <- hypso_ff %>%
+  filter(area < 1e7) %>%
+  mutate(sizeclass = floor(log10(area))) %>%
+  select(llid, form_factor, area, sizeclass) %>%
+  rename(lagoslakeid=llid) %>%
+  mutate(group="Stachelek lakes")
+
+# compare the distribution of form factors with their real data
+rbind(
+  obs_df_sample,
+  stachelek_sample
+) %>%
   ggplot(aes(x=form_factor, y=group)) + geom_density_ridges() +
   xlim(0, 4)
+
+# compare the area distributions
+rbind(
+  obs_df_sample,
+  stachelek_sample
+) %>%
+  ggplot(aes(x=area, y=group)) + geom_density_ridges() +
+  scale_x_log10()
+
+obs_df_sample %>% pull(sizeclass) %>% table()
+stachelek_sample %>% pull(sizeclass) %>% table()
+
+obs_df_sample %>% pull(form_factor) %>% median()
