eaa99be767121695de72b1453f4abc2aafbc73a0
s-kganz <ganzkeenan1@gmail.com>
2023-02-22 17:37:26 -0800

    use more explicit dirnames in compile_model_results

--- scripts/compile_model_results.R
+++ scripts/compile_model_results.R
@@ -5,17 +5,17 @@ library(tidyverse)
 # and modeled depth. This file takes the modeling results and combines everything
 # together.
 depths <- read_csv("data_in/lagos/LAGOS_US_LOCUS/lake_depth.csv")
-zmax   <- read_csv("data_out/model_results/maxdepth/maxdepth_prediction_results.csv")
-zmean  <- read_csv("data_out/model_results/meandepth/meandepth_prediction_results.csv")
+zmax   <- read_csv("data_out/model_results/maxdepth_lt1000ha/maxdepth_prediction_results.csv")
+zmean  <- read_csv("data_out/model_results/meandepth_lt1000ha/meandepth_prediction_results.csv")
 
 model_comp <- zmax %>%
   select(lagoslakeid, in_training, contains("prediction")) %>%
-  rename_with(~ str_c("maxdepth_", .x), .cols=contains("prediction")) %>%
+  rename_with(~ str_c("maxdepth_m_", .x), .cols=contains("prediction")) %>%
   rename(in_maxdepth_training=in_training) %>%
   inner_join(
     zmean %>%
       select(lagoslakeid, in_training, contains("prediction")) %>%
-      rename_with(~ str_c("meandepth_", .x), .cols=contains("prediction")) %>%
+      rename_with(~ str_c("meandepth_m_", .x), .cols=contains("prediction")) %>%
       rename(in_meandepth_training=in_training),
     by="lagoslakeid"
   ) %>%
@@ -30,10 +30,10 @@ model_comp <- zmax %>%
   ) %>%
   # pick out the best max/mean depth depending on if an observed one is available
   mutate(
-    best_maxdepth  = ifelse(is.na(lake_maxdepth_m), maxdepth_prediction_rf,
+    best_maxdepth  = ifelse(is.na(lake_maxdepth_m), maxdepth_m_prediction_rf,
                             lake_maxdepth_m),
-    best_meandepth = ifelse(is.na(lake_meandepth_m), meandepth_prediction_rf,
+    best_meandepth = ifelse(is.na(lake_meandepth_m), meandepth_m_prediction_rf,
                             lake_meandepth_m),
     cone_volume = best_maxdepth * area / 3
   ) %>%
-  write_csv("data_out/model_results/compiled_predictions.csv")
+  write_csv("data_out/model_results/lt1000ha_compiled_predictions.csv")
